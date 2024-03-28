3b7a8b35a060cbf87e61324778b4543d78a939b7
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Pro.local>
2024-03-28 08:37:20 -0700

    simplify server logic

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/menothe/go-server
+
+go 1.22.1
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -3,23 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+type Product struct {
+	Id          uuid.UUID `json:"id"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	Seller      string    `json:"seller"`
+	Category    string    `json:"category"`
 }
 
-func infoHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Path: %s\n", r.URL.Path)
-	fmt.Fprintf(w, "Method: %s\n", r.Method)
+func getProductHandler(w http.ResponseWriter, r *http.Request) {
+	product := &Product{
+		Id:          uuid.New(),
+		Description: "Old Leather Boots, size 11, black",
+		Price:       110.34,
+		Seller:      "sellerRuss",
+		Category:    "Shoes",
+	}
+	fmt.Println(*product)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	// Register handlers for specific routes
-	mux.HandleFunc("/", helloHandler)
-	mux.HandleFunc("/info", infoHandler)
+	mux.HandleFunc("GET /message", getProductHandler)
 
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", mux)
