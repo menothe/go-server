0da04070ce24daa814915e8feba94a4246cebfde
Paul Dubovsky <pauldubovsky@gmail.com>
2024-03-28 20:04:49 -0700

    added support for saving new data to local filesystem

---
+++ handlers/handlers.go
@@ -0,0 +1,106 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	Id          uuid.UUID `json:"id"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	Seller      string    `json:"seller"`
+	Category    string    `json:"category"`
+}
+
+func GetProductHandler(w http.ResponseWriter, r *http.Request) {
+	product := &Product{
+		Id:          uuid.New(),
+		Description: "Old Leather Boots, size 11, black",
+		Price:       110.34,
+		Seller:      "sellerRuss",
+		Category:    "Shoes",
+	}
+	fmt.Println(*product)
+}
+
+func CreateProductHandler(w http.ResponseWriter, r *http.Request) {
+	var newProduct = &Product{}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+
+	// Process the request body based on content type
+	// (demonstrated with JSON data processing)
+	if err := json.Unmarshal(body, newProduct); err != nil {
+		// Handle invalid JSON format
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
+	newProduct.Id = uuid.New()
+
+	err = saveData(newProduct)
+	if err != nil {
+		fmt.Println("Unable to create product:", err)
+		return
+	}
+
+	fmt.Println("product created: ", *newProduct)
+}
+
+func FetchAllProductsHandler(w http.ResponseWriter, r *http.Request) {
+	var products []Product
+
+	err := readUsersFromFile(&products, "./products.json")
+	if err != nil {
+		fmt.Println("Error reading data from file:", err)
+		return
+	}
+	fmt.Println("all products: ", products)
+}
+
+// Helper function to read existing data (modify as needed)
+func readUsersFromFile(products *[]Product, filename string) error {
+	b, err := os.ReadFile(filename)
+	if err != nil && os.IsNotExist(err) {
+		// File doesn't exist, initialize empty slice
+		*products = []Product{}
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, products)
+}
+
+func saveData(product *Product) error {
+	var products []Product
+
+	err := readUsersFromFile(&products, "./products.json")
+	if err != nil {
+		fmt.Println("Error reading data from file:", err)
+		return err
+	}
+	products = append(products, *product)
+
+	data, err := json.Marshal(products)
+	if err != nil {
+		fmt.Println("Error marshalling data:", err)
+		return err
+	}
+	err = os.WriteFile("./products.json", data, 0644)
+	if err != nil {
+		fmt.Println("Error writing data to file:", err)
+		return err
+	}
+	fmt.Println("Data persisted to products.json")
+	return nil
+}
--- main.go
+++ main.go
@@ -1,59 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
-	"github.com/google/uuid"
+	"github.com/menothe/go-server/handlers"
 )
-
-type Product struct {
-	Id          uuid.UUID `json:"id"`
-	Description string    `json:"description"`
-	Price       float64   `json:"price"`
-	Seller      string    `json:"seller"`
-	Category    string    `json:"category"`
-}
-
-func getProductHandler(w http.ResponseWriter, r *http.Request) {
-	product := &Product{
-		Id:          uuid.New(),
-		Description: "Old Leather Boots, size 11, black",
-		Price:       110.34,
-		Seller:      "sellerRuss",
-		Category:    "Shoes",
-	}
-	fmt.Println(*product)
-}
-
-func createProductHandler(w http.ResponseWriter, r *http.Request) {
-	var newProduct = &Product{}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-
-	// Process the request body based on content type
-	// (demonstrated with JSON data processing)
-	if err := json.Unmarshal(body, &newProduct); err != nil {
-		// Handle invalid JSON format
-		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
-		return
-	}
-	newProduct.Id = uuid.New()
-	fmt.Println("product created: ", *newProduct)
-}
 
 func main() {
 	mux := http.NewServeMux()
 
 	// Register handlers for specific routes
-	mux.HandleFunc("GET /product", getProductHandler)
-	mux.HandleFunc("POST /product", createProductHandler)
+	mux.HandleFunc("GET /product", handlers.GetProductHandler)
+	mux.HandleFunc("POST /product", handlers.CreateProductHandler)
+	mux.HandleFunc("GET /products", handlers.FetchAllProductsHandler)
 
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", mux)
@@ -62,5 +22,9 @@ func main() {
 /*
 curl -X POST localhost:8080/product \
 	 -H "Content-Type: application/json" \
-	 -d '{"description": "Harry Potter and the Sorcerers Stone", "price": 19.99, "seller": "sellerBob", "category": "books"}'
+	 -d '{"description": "The Matrix", "price": 9.99, "seller": "sellerTom", "category": "movies"}'
+
+curl -X POST localhost:8080/product \
+	 -H "Content-Type: application/json" \
+	 -d '{"description": "Harry Potter and the Sorcerers Stone", "price": 19.99, "seller": "sellerRuss", "category": "books"}'
 */
